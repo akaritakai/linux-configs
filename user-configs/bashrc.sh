0916c51596e515dfc3ab83cf7de516a06ad97674
Justin Kaufman <akaritakai@gmail.com>
2014-06-08 18:29:34 -0500

    Update bashrc.sh

--- user-configs/bashrc.sh
+++ user-configs/bashrc.sh
@@ -9,6 +9,10 @@ function add_gems() {
   local ruby_ver=`ruby --version | awk '{print $2;}' | sed 's/^\(.*\)[A-Za-z].*$/\1/'`
   if [ -d "$HOME/.gem/ruby/$ruby_ver/bin" ]; then
     PATH="$HOME/.gem/ruby/$ruby_ver/bin:$PATH"
+  elif [[ "$ruby_ver" == "1.9.3" ]]; then
+    if [ -d "$HOME/.gem/ruby/1.9.1/bin" ]; then
+      PATH="$HOME/.gem/ruby/1.9.1/bin:$PATH"
+    fi
   fi
 }
 add_gems
