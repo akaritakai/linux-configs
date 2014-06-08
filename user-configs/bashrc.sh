8d1fefa12fe64d08c50bb5feca2579e0f45c0a12
Justin Kaufman <akaritakai@gmail.com>
2014-06-08 18:26:21 -0500

    Update bashrc.sh

--- user-configs/bashrc.sh
+++ user-configs/bashrc.sh
@@ -3,3 +3,12 @@
 
 # GPG Exports
 export GPGKEY=somekeyid
+
+# Add Ruby gems to the path
+function add_gems() {
+  local ruby_ver=`ruby --version | awk '{print $2;}' | sed 's/^\(.*\)[A-Za-z].*$/\1/'`
+  if [ -d "$HOME/.gem/ruby/$ruby_ver/bin" ]; then
+    PATH="$HOME/.gem/ruby/$ruby_ver/bin:$PATH"
+  fi
+}
+add_gems
