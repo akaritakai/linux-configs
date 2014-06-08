45d2f3528036ba2f3b7da700ea7bab063adc5505
Justin Kaufman <akaritakai@gmail.com>
2014-06-08 02:36:22 -0500

    Update profile.sh

--- user-configs/profile.sh
+++ user-configs/profile.sh
@@ -1,19 +1,8 @@
 # These are additions to the ~/.profile file that I have found helpful
 # Do /NOT/ replace your entire ~/.profile file with this
 
-# Check to see if SSH agent is already running
-agent_pid="$(ps -ef | grep "ssh-agent" | grep -v "grep" | awk '{print($2)}')"
-
-# Start SSH agent if not running
-if [[ -z "$agent_pid" ]]; then
-    eval "$(ssh-agent)"
-    ssh-add
-else # SSH agent is already running
-    agent_ppid="$(($agent_pid - 1))"
-    agent_sock="$(find /tmp -path "*ssh*" -type s -iname "agent.$agent_ppid")"
-    export SSH_AGENT_PID="$agent_pid"
-    export SSH_AUTH_SOCK="$agent_sock"
-fi
+# Sets up ssh-agent and gpg-agent with inheritance
+eval $(keychain --eval --quiet)
 
 # Add Ruby Gems to the path
 for ruby_ver in $HOME/.gem/ruby/*; do
