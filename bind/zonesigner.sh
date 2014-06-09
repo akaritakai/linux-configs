85d87d1b58fdb1b60f82f46e27ed781ef5bcc635
Justin Kaufman <akaritakai@gmail.com>
2014-06-09 00:28:13 -0500

    Update zonesigner.sh

--- bind/zonesigner.sh
+++ bind/zonesigner.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 function update_serial() {
-  local serial=`/usr/sbin/named-checkzone some.domain /etc/bind/db.some.domain | grep -Eho '[0-9]{10}'`
+  local current_serial=`/usr/sbin/named-checkzone some.domain /etc/bind/db.some.domain | grep -Eho '[0-9]{10}'`
   local today=`date +%Y%m%d`
-  if [[ $serial != $today* ]]; then
-    sed -i 's/'$serial'/'$serial00'/' /etc/bind/db.some.domain
+  if [[ $current_serial != $today* ]]; then
+    sed -i 's/'$current_serial'/'$today'00/' /etc/bind/db.some.domain
+  else
+    sed -i 's/'$current_serial'/'$(($current_serial+1))'/' /etc/bind/db.some.domain
   fi
 }
 
-update_serial
+readonly current_dir=`pwd`
+cd /etc/bind
 /usr/sbin/dnssec-signzone -A -3 $(head -c 1000 /dev/random | sha1sum | cut -b 1-16) -N INCREMENT -o some.domain -t /etc/bind/db.some.domain
 service bind9 restart
+cd $current_dir
