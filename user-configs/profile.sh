07d44820ec92e35a1139164a06741160d97fe2c6
Justin Kaufman <akaritakai@gmail.com>
2014-06-24 09:18:52 -0500

    Update profile.sh

--- user-configs/profile.sh
+++ user-configs/profile.sh
@@ -2,4 +2,6 @@
 # Do /NOT/ replace your entire ~/.profile file with this
 
 # Sets up ssh-agent and gpg-agent with inheritance
+# Make sure to specify after --eval what should be remembered
+# a.k.a. --eval id_rsa AB1234C (where id_rsa is your SSH key and AB1234C is your GPG key)
 eval $(keychain --eval --quiet)
